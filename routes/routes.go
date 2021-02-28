fa7c34de52b83b47653f703cfb9eb142d3da3edb
Febrari Supaldi <febrarisupaldi@outlook.com>
2021-02-28 21:56:49 +0700

    make connection mysql and setting echo

---
+++ routes/routes.go
@@ -0,0 +1,21 @@
+package routes
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func Init() *echo.Echo {
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!\n")
+	})
+
+	return e
+}
